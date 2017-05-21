1da38a27bb41473c784fd0e169ced1e17daefb09
Matthias Geisler <matthias@openwebcraft.com>
2017-05-21 14:01:46 +0200

    needed to adjust the permissions because docker mounts volumes as root

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,13 @@
 set -e
 
 if [ "$1" = 'caddy' ]; then
+	# we need to set the permissions here because docker mounts volumes as root
+	chown -R caddy:caddy /var/lib/caddy
+
+	chmod -R 0770 /var/lib/caddy
+
+	chmod 664 /var/lib/caddy/Caddyfile
+
 	exec gosu caddy "$@"
 fi
 
